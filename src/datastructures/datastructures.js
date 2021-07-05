1be53e142f2ba0b799d1701b9bb0729c83ce7ce3
James Miller <james@james-miller.co.uk>
2021-07-05 14:32:34 +0100

    feat: Add datastructure test questions

---
+++ src/datastructures/datastructures.js
@@ -0,0 +1,9 @@
+export const addTwoArrays = (arr1, arr2) => {};
+
+export const removeEvenNumbers = (arr) => {};
+
+export const locateNumber = (arr, numberToLocate) => {};
+
+export const calculateTotal = (arr) => {};
+
+export const sortedArrayOfObjects = (arrayOfObjects) => {};
